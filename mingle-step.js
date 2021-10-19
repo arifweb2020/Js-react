19ab0a10336bb4d4edd208bd3569e99b949549e1
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2021-10-19 12:25:53 +0530

    Create mingle-step.js

---
+++ mingle-step.js
@@ -0,0 +1,40 @@
+1> npm init -- for initilize the server
+
+2> npm install express - 
+
+3> to run app -> node app and browser : http://localhost:5000/
+
+4> npm install nodemon -> nodemon app
+
+5> MiddleWare - it modify the request b4 it reaches to actucal route handler
+
+6> MONGODB
+
+a> signin mongoDB cloud
+
+b> create project
+
+c> Build Cluster or database - choose free one
+
+d> Network Access - Add IP Address - Allow access from anywhere
+
+e> Database Access - add newdatabase user (arif21 , pass - 265DFTLC6W0wytXP)
+
+f> go to database - go to connect - connect yr application 
+save this file in yr project
+mongodb+srv://arif21:<password>@cluster0.3lae7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority 
+
+7> npm i mongoose - for database
+
+8> npm i bcryptjs - for password encryption
+
+9> npm i jsonwebtoken - for protected resources
+
+10> image store - https://cloudinary.com/
+
+11> sendgrid for email pass-8660790990arif2021, 
+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
+SG.nB7lW3CfRQ6S6nsZpR0hrw.Rd-T_ve61Fw66wpkGYGjSvEJyM2-F1rba2-ZTLOG0gk- admin@sky
+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
+
+12> npm install nodemailer nodemailer-sendgrid-transport
