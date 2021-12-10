006f4fbdd5847516aedb7e9568edc4a086a00e59
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2021-12-10 09:57:32 +0530

    Create object-condition-react.js

---
+++ object-condition-react.js
@@ -0,0 +1,6 @@
+{
+              Object.keys(addQueryData).length == 0 ?
+                <AddQuery />
+                :
+                <button className="disableBtn" disabled>{approvalLabel.fieldsLabel.addQuery}</button>
+            }
