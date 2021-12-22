dc38097a4a11c4815db36bf37ad9a17b7c9f4b97
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2021-12-22 17:46:08 +0530

    Update Docker-step.js

--- Docker-step.js
+++ Docker-step.js
@@ -25,3 +25,65 @@
 	@rootw44:/app#
 	
 	@rootw44:/app# exit // for exit
+	
+	
+	// Dockerfile
+	# pulling official base image
+FROM node:14.18.0
+
+# seting working directory
+WORKDIR /app
+
+# adding `/app/node_modules/.bin` to $PATH
+ENV PATH /app/node_modules/.bin:$PATH
+
+# installing app dependencies
+COPY package.json ./
+COPY package-lock.json ./
+RUN npm install --silent
+RUN npm install react-scripts@4.0.3 -g --silent
+
+# adding all files to app
+COPY . ./
+
+# starting the app
+CMD ["npm", "start"]
+
+//Dockerfile.prod
+
+# # build environment
+# FROM node:16.13.0 as build
+# WORKDIR /app
+# ENV PATH /app/node_modules/.bin:$PATH
+# COPY package.json ./
+# COPY package-lock.json ./
+# RUN npm ci --silent
+# RUN npm install react-scripts@3.4.1 -g --silent
+# COPY . ./
+# RUN npm run build
+
+# # production environment
+# FROM nginx:stable-alpine
+# COPY --from=build /app/build /usr/share/nginx/html
+# EXPOSE 80
+# CMD ["nginx", "-g", "daemon off;"]
+FROM node:14-alpine as build-step
+RUN mkdir /app
+WORKDIR /app
+COPY package.json /app
+RUN npm install
+COPY . /app
+RUN npm run build
+FROM nginx
+COPY default.conf /etc/nginx/conf.d/default.conf
+COPY --from=build-step /app/build /etc/nginx/html/arif/
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
+	
+	// .dockerignore file
+	
+	node_modules
+build
+.dockerignore
+Dockerfile
+Dockerfile.prod
