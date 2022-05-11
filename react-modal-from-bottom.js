46e3198ce90e538184af5516218912752d1e4d8a
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2022-05-11 14:48:05 +0530

    Update react-modal-from-bottom.js

--- react-modal-from-bottom.js
+++ react-modal-from-bottom.js
@@ -84,3 +84,89 @@ function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+
+
+
+/**
+ * Modal for view the feedback comments.
+ * autor : Arif
+ */
+ import React from 'react';
+ import Box from '@mui/material/Box';
+ import Typography from '@mui/material/Typography';
+ import Modal from '@mui/material/Modal';
+ import styled from "styled-components";
+ import TextareaAutosize from '@mui/material/TextareaAutosize';
+ //import { ClosedIcon } from '../assets/icons/feedBackIcon';
+ 
+ const style = {
+    //  position: 'absolute',
+    //  top: '50%',
+    //  left: '50%',
+    //  transform: 'translate(-50%, -50%)',
+    //  width: 700,
+    //  bgcolor: 'background.paper',
+    //  boxShadow: 24,
+    //  p: 2,
+    //  borderRadius: '20px',
+    position: 'fixed',
+  bottom: 0,
+  backgroundColor: '#fff',
+  width: '100%',
+  left: 0,
+  right:0,
+  //padding: '0 12px 12px',
+  transition: 'bottom 0.3s ease-out',
+  zIndex:10,
+  height:'400px',
+  borderTopLeftRadius: '20px',
+  borderTopRightRadius: '20px',
+  outline: '#fff'
+ };
+ 
+ const ViewComments = styled(Typography)`
+ color: #CA0000;
+ font-weight:500;
+ text-decoration:underline;
+ cursor:pointer;
+ `;
+
+ const CloseButton = styled(Box)`
+ display:flex;
+ justify-content:end;
+ & span{
+     cursor:pointer;
+ }
+  `
+ /**
+  * 
+  * @param {string} viewComments 
+  * @returns BottomModal Component
+  */
+ function BottomModal(props) {
+     const [open, setOpen] = React.useState(false);
+     const handleOpen = () => setOpen(true);
+     const handleClose = () => setOpen(false);
+     return (
+         <div>
+             <ViewComments onClick={handleOpen}>view comments</ViewComments>
+             <Modal
+                 open={open}
+                 aria-labelledby="modal-modal-title"
+                 aria-describedby="modal-modal-description"
+             >
+                 <Box sx={style}>
+                     <CloseButton  >
+                         <span onClick={handleClose}>
+                             close
+                         </span>
+                     </CloseButton>
+                     <h1>sd</h1>
+                 </Box>
+             </Modal>
+         </div>
+     );
+ }
+ 
+ export default BottomModal;
