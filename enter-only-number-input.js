0dce0ecf0d3443d0e84e95b4593a75c40a36b0c3
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2022-05-13 11:51:42 +0530

    Update enter-only-number-input.js

--- enter-only-number-input.js
+++ enter-only-number-input.js
@@ -3,3 +3,19 @@
                                         event.preventDefault();
                                     }
                                 }} />
+
+
+// 2nd way
+
+const [age, setAge] = useState();
+
+const handleChange = (e) => {
+  const value = e.target.value.replace(/\D/g, "");
+  setAge(value);
+};
+
+return (
+  <div>
+    <input value={age} onChange={handleChange} />
+  </div>
+);
